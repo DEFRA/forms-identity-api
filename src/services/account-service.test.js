16173d3e986e83b2559e7a4708dc36d50cf9bd45
Jez Barnsley <jbarnsley.github@gmail.com>
2026-09-23 15:57:09 +0100

    Fixed test

--- src/services/account-service.test.js
+++ src/services/account-service.test.js
@@ -9,18 +9,41 @@ describe('account service', () => {
   const uid = 'uid-1'
   const id = 'acc-id'
   const email = 'new-email@test.com'
-  const phone = '+447507123456'
+  const validOtp = {
+    consumed: false,
+    verified: true,
+    accountId: id,
+    target: email
+  }
 
   it('should throw when account not found', async () => {
     jest.mocked(findById).mockResolvedValueOnce(null)
+    jest
+      .mocked(findOne)
+      // @ts-expect-error - partial mock of test data
+      .mockResolvedValueOnce(validOtp)
+      // @ts-expect-error - partial mock of test data
+      .mockResolvedValueOnce(validOtp)
     const res = await updateEmail(uid, id)
     expect(res).toEqual({ status: 'invalid' })
   })
 
-  it('should return invalid when OTP not found', async () => {
+  it('should return invalid when email OTP not found', async () => {
     // @ts-expect-error - partial mock of test data
     jest.mocked(findById).mockResolvedValueOnce({ status: 'active' })
     jest.mocked(update).mockResolvedValueOnce(false)
+    const res = await updateEmail(uid, id)
+    expect(res).toEqual({ status: 'invalid' })
+  })
+
+  it('should return invalid when phone OTP not found', async () => {
+    // @ts-expect-error - partial mock of test data
+    jest.mocked(findById).mockResolvedValueOnce({ status: 'active' })
+    jest.mocked(update).mockResolvedValueOnce(false)
+    jest
+      .mocked(findOne)
+      // @ts-expect-error - partial mock of test data
+      .mockResolvedValueOnce(validOtp)
     const res = await updateEmail(uid, id)
     expect(res).toEqual({ status: 'invalid' })
   })
@@ -29,22 +52,13 @@ describe('account service', () => {
     // @ts-expect-error - partial mock of test data
     jest.mocked(findById).mockResolvedValueOnce({ status: 'active' })
     jest.mocked(update).mockResolvedValueOnce(true).mockResolvedValueOnce(true)
+    jest.mocked(findOne)
     jest
       .mocked(findOne)
       // @ts-expect-error - partial mock of test data
-      .mockResolvedValueOnce({
-        consumed: false,
-        verified: true,
-        accountId: id,
-        target: email
-      })
+      .mockResolvedValueOnce(validOtp)
       // @ts-expect-error - partial mock of test data
-      .mockResolvedValueOnce({
-        consumed: false,
-        verified: true,
-        accountId: id,
-        target: phone
-      })
+      .mockResolvedValueOnce(validOtp)
     const res = await updateEmail(uid, id)
     expect(res).toEqual({ status: 'valid' })
   })
